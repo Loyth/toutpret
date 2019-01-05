e246f8b6a8f6244278c94b67cb39afd7f204be95
Thomas Loyau <thoomas.loyau@gmail.com>
2019-01-05 14:40:07 +0100

    start code category activity

---
+++ app/src/main/java/toutpret/isep/com/toutpret/categories/CategoriesActivity.java
@@ -0,0 +1,15 @@
+package toutpret.isep.com.toutpret.categories;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import toutpret.isep.com.toutpret.R;
+
+public class CategoriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categories);
+    }
+}
