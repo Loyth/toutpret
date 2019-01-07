be28cbe9719311179fd71ac5a4a3244ce187eb11
Hichem <add.hichem@gmail.com>
2019-01-07 18:12:08 +0100

    Affichage de l'icone "serviette" du client dans la map

--- app/src/main/java/toutpret/isep/com/toutpret/map/DialogFragmentMap.java
+++ app/src/main/java/toutpret/isep/com/toutpret/map/DialogFragmentMap.java
@@ -12,8 +12,15 @@ import android.view.ViewGroup;
 import android.view.WindowManager;
 
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.Icon;
+import com.mapbox.mapboxsdk.annotations.IconFactory;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
+import toutpret.isep.com.toutpret.MainActivity;
 import toutpret.isep.com.toutpret.R;
 
 public class DialogFragmentMap extends DialogFragment {
@@ -24,17 +31,40 @@ public class DialogFragmentMap extends DialogFragment {
                              Bundle savedInstanceState) {
         Mapbox.getInstance(getContext(), "pk.eyJ1IjoiaGljaGVtYWRkIiwiYSI6ImNqcWw4aGN4ZjB6NG40M25uMmgxYjNzZzIifQ.h8eqs-0JnQoOFVYIOzl4dw");
 
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
         //WindowManager.LayoutParams wmlp = getDialog().getWindow().getAttributes();
         // wmlp.gravity = Gravity.FILL_HORIZONTAL;
 
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        mapView = view.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        /*
+        ///// Récupérer la location du client
+        Location locationEngine = new LostLocationEngine(MainActivity.this);
+        locationEngine.setPriority(LocationEnginePriority.HIGH_ACCURACY);
+        locationEngine.setInterval(5000);
+        locationEngine.activate();
+        Location lastLocation = locationEngine.getLastLocation();
+        */
+
+        ///// Mettre le marqueur
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+                IconFactory iconFactory = IconFactory.getInstance(getContext());
+                Icon icon = iconFactory.fromResource(R.drawable.towel_small);
+
+                mapboxMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(48.13863, 11.57603))
+                        .icon(icon));
+            }
+        });
+        return view;
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        mapView = view.findViewById(R.id.mapView);
-        mapView.onCreate(savedInstanceState);
         mapView.onStart();
     }
 
